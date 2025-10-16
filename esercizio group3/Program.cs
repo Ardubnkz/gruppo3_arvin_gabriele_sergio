86bbc58decd3b786652a35a8039b0bd16be22089
Arvin <arvin.nikzad@gmail.com>
2025-10-16 14:38:48 +0200

    up

---
+++ esercizio group3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace esercizio_group3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
