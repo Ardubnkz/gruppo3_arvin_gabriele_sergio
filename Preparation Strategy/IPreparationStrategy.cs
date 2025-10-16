bb79c7c5378802efb2043ba95089dbfc8f06dd4b
LandfillHeart <kimbleredward@gmail.com>
2025-10-16 16:56:11 +0200

    Preparation Strategy, Aggiornamenti da altri branch
    - Aggiunto cambiamenti presi da altri branch (IBevanda, Registro Ordini etc)
    - PreparationContext può ricevere una strategia esistente o un enum per scegliere quale strategia da applicare nella preparazione del drink
    - PreparationStrategy riceve un drink e stampa il suo nome e come viene preparato

---
+++ Preparation Strategy/IPreparationStrategy.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_group3.Preparation_Strategy
+{
+	public enum PreparationType
+	{
+		None,
+		Shaked,
+		Mixed, 
+		OnTheRocks
+	}
+
+	public interface IPreparationStrategy
+	{
+		public PreparationType Type { get; }
+		public void Prepare(IBevanda drink);
+	}
+
+	public class ShakedPreparation : IPreparationStrategy
+	{
+		public PreparationType Type => PreparationType.Shaked;
+		public void Prepare(IBevanda drink)
+		{
+			Console.WriteLine($"Il barista sta preparando un {drink.Descrizione()} shakerato");
+		}
+	}
+
+	public class MixedPreparation : IPreparationStrategy
+	{
+		public PreparationType Type => PreparationType.Mixed;
+		public void Prepare(IBevanda drink)
+		{
+			Console.WriteLine($"Il barista sta preparando un {drink.Descrizione()} mescolato");
+		}
+	}
+
+	public class OnTheRocksPreparation : IPreparationStrategy
+	{
+		public PreparationType Type => PreparationType.Shaked;
+		public void Prepare(IBevanda drink)
+		{
+			Console.WriteLine($"Il barista sta preparando un {drink.Descrizione()} on the rocks");
+		}
+	}
+
+	public class PreparationContext
+	{
+		private IPreparationStrategy strategy;
+
+		public void SetStrategy( IPreparationStrategy strategy )
+		{
+			this.strategy = strategy;
+		}
+
+		public void SetStrategy(PreparationType preparationType)
+		{
+			strategy = preparationType switch
+			{
+				PreparationType.Shaked => new ShakedPreparation(),
+				PreparationType.Mixed => new MixedPreparation(),
+				PreparationType.OnTheRocks => new OnTheRocksPreparation(),
+			};
+		}
+
+		public void PrepareDrink(IBevanda toPrepare)
+		{
+			if(toPrepare == null)
+			{
+				Console.WriteLine("Non hai inserito un drink da preparare");
+				return;
+			}
+
+			if (strategy == null)
+			{
+				Console.WriteLine($"Il barista non può ancora preparare il tuo {toPrepare.Descrizione()}, non gli hai detto come farlo!");
+				return;
+			}
+
+			strategy.Prepare(toPrepare);
+		}
+	}
+
+}
---
+++ RegistroOrdini.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_group3
+{
+	public class RegistroOrdini
+	{
+		private static RegistroOrdini instance;
+		private RegistroOrdini() { }
+		public static RegistroOrdini GetInstance()
+		{
+			if (instance == null)
+			{
+				instance = new RegistroOrdini();
+			}
+			return instance;
+		}
+		public void RegistraOrdine(string DescrizioneBevanda)
+		{
+			Console.WriteLine($"Ordine registrato: {DescrizioneBevanda}");
+		}
+		public void AggiungiOrdine(string DescrizioneBevanda)
+		{
+			Console.WriteLine($"Ordine aggiunto al registro: {DescrizioneBevanda}");
+		}
+	}
+}
