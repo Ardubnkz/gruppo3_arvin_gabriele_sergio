6c420f0eb676af98fcb6b5ed11b9ea0a2231743f
Arvin <arvin.nikzad@gmail.com>
2025-10-16 15:10:05 +0200

    Interface

--- esercizio group3/Program.cs
+++ esercizio group3/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace esercizio_group3
 {
+    interface IprepataionStrategy
+    {
+        void Prepare(string nomeBevanda);
+    }
+    interface Ibevanda
+    {
+        string Getdescrizione();
+        double Getcosto();
+    }
+    interface IObserver
+    {
+        void Notifica(string message);
+    }
+    interface Isoggeto
+    {
+        void registra(IObserver o);
+        void rimuovi(IObserver o);
+        void notificaTutti(string message);
+
+    }
+    interface ICliente
+    {
+        void OrdinaPronto(string DescrizioneBevanda);
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
