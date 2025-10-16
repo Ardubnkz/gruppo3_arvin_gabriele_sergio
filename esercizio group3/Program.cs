acd3592a6d057ed4e3fb2053bebdbb2aa8a38076
DemonBloodstorm <gabrielefrangiosa8@gmail.com>
2025-10-16 15:35:39 +0200

    Creazione del observer e inizio del main
    Creazione di un observer che permette di aggiungere clienti e notificare loro.

--- esercizio group3/Program.cs
+++ esercizio group3/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace esercizio_group3
+using System;
+using System.Collections.Generic;   
+
+namespace esercizio_group3
 {
-    internal class Program
+    public interface IObserver
+    {
+        void Update();
+    }
+
+    public interface IObservable
+    {
+        void AggiungiCliente(IObserver observer);
+        void RimuoviCliente(IObserver observer);
+        void NotificaClienti();
+    }
+
+    public class Cliente : IObserver
+    {
+        public string Nome { get; }
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+        public void Update()
+        {
+            Console.WriteLine($"Benvenuto {Nome}, al Pub C#");
+        }
+    }
+    public class Pub : IObservable
+{
+    private readonly List<IObserver> _clienti = new List<IObserver>();
+
+    public void Attach(IObserver observer)
+    {
+        _clienti.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        _clienti.Remove(observer);
+    }
+
+    public void Notify(string message)
+    {
+        foreach (var cliente in _clienti)
+            cliente.Update(message);
+    }
+}
+
+
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pub pub = new Pub();
+            Cliente cliente1 = new Cliente("Gabriele");
+            Cliente cliente2 = new Cliente("Arvin");
+            Cliente cliente3 = new Cliente("Sergio");
+            pub.AggiungiCliente(cliente1);
+            pub.AggiungiCliente(cliente2);
+            pub.AggiungiCliente(cliente3);
+            pub.NotificaClienti();
         }
     }
 }
